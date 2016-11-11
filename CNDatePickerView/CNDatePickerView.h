072cd28c1c29cfa009a883b98389201903baed3b
Igor Smirnov <dev_ios@ronin.pro>
2016-11-11 18:18:24 +0300

    Initial Commit

---
+++ CNDatePickerView/CNDatePickerView.h
@@ -0,0 +1,19 @@
+//
+//  CNDatePickerView.h
+//  CNDatePickerView
+//
+//  Created by Igor Smirnov on 11/11/2016.
+//  Copyright © 2016 Complex Numbers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CNDatePickerView.
+FOUNDATION_EXPORT double CNDatePickerViewVersionNumber;
+
+//! Project version string for CNDatePickerView.
+FOUNDATION_EXPORT const unsigned char CNDatePickerViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CNDatePickerView/PublicHeader.h>
+
+
